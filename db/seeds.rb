df3abf7eefabc8caecf8a97d86b4532728162e63
Narendra Sharma <narendra_sharma@external.mckinsey.com>
2019-06-28 00:15:21 +0530

    adds seed data to start with

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# Sick Leave|Annual Leave|Maternity Leave
+company = Company.create(name: 'Alphabet')
+policies = Policy.create([
+                             {name: 'Sick Leave', company_id: company.id},
+                             {name: 'Annual Leave', company_id: company.id},
+                              {name: 'Maternity Leave', company_id: company.id}
+                         ])
+
+employee = Employee.create(name: 'Green Arrow', email: 'green_arrow@abc.com', phone: 9999999999, company_id: company.id)
+
+employee.policies << policies
+employee.save!
